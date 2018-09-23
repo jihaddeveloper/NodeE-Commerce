17db15db57487cd157c445800731bbff2588c5f5
jihaddeveloper <jihad.developer@gmail.com>
2018-09-23 18:21:47 +0600

    Added Authentication.

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+
+var userSchema = new Schema({
+    name: { type: String, index: true, required: true, maxlength: 30, minlength: 5 },
+    username: { type: String, lowercase: true, required: true, maxlength: 20, minlength: 5 },
+    email: { type: String, required: true, lowercase: true, unique: true },
+    password: { type: String,required: true, minlength: 5 }
+});
+
+const User = module.exports = mongoose.model('User', userSchema);
+
+//Create New User
+module.exports.createUser = function(newUser, callback){
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+}
+
+//To get User by Id
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+//To get User by Username
+module.exports.getUserByUsername = function(username, callback){
+    var query = {username : username}
+    User.findOne(query, callback);
+}
+
+
+//To get User by Name
+module.exports.getUserByName = function(name, callback){
+    var query = {name : name}
+    User.findOne(query, callback);
+}
+
+//Password comparetor
+module.exports.comparePassword = function(candidatePassword, hashPassword, callback){
+    bcrypt.compare(candidatePassword, hashPassword, function(err, isMatch){
+        if(err) throw err;
+        callback(null, isMatch);
+    });
+}
--- routes/userController.js
+++ routes/userController.js
@@ -1,14 +1,113 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-//Register
-router.get('/register', function(req, res, next) {
+const User = require('../models/user');
+
+//Register Forn View
+router.get('/register', function (req, res, next) {
     res.render('register');
 });
 
-//Login
-router.get('/login', function(req, res, next) {
+//Register Action
+router.post('/register', function (req, res, next) {
+
+    //Getting Input from View Page
+    var name = req.body.name;
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    //console.log(name);
+
+    //Validation
+    req.checkBody('name', 'Name is reqiured').notEmpty();
+    req.checkBody('username', 'Username is reqiured').notEmpty();
+    req.checkBody('email', 'Email is reqiured').notEmpty();
+    req.checkBody('email', 'Email is reqiured').isEmail();
+    req.checkBody('password', 'Password is reqiured').notEmpty();
+    req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        console.log('Errors');
+        res.render('register', {
+            errors: errors
+        });
+    } else {
+        newUser = new User({
+            name: name,
+            username: username,
+            email: email,
+            password: password
+        });
+
+        User.createUser(newUser, function (err, User) {
+            if (err) throw err;
+            console.log(User);
+        });
+
+        req.flash('success_msg', 'You are registered now and can login');
+
+        res.redirect('/users/login');
+    }
+});
+
+//Login Form View
+router.get('/login', function (req, res, next) {
     res.render('login');
+});
+
+//Login Authentication
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.getUserByUsername(username, function (err, user) {
+            if (err) {
+                throw err;
+            }
+            if (!user) {
+                return done(null, false, {message: 'Incorrect username.'});
+            }
+            User.comparePassword(password, user.password, function(err, isMatch){
+                if(err) throw err;
+                if(isMatch){
+                    return done(null, user);
+                }else{
+                    return done(null, false, {message: 'Incorrect password.'});
+                }
+            });
+        });
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+    User.getUserById(id, function(err, user){
+        done(err, user);
+    });
+});
+
+router.post('/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    }),
+    function (req, res) {
+        res.redirect('/');
+    });
+
+
+//Logout
+router.get('/logout', function(req, res){
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
 });
 
 module.exports = router;
